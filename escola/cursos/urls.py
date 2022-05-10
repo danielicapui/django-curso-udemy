64570487a6e9c31ef7c46e2d5ac24d4ea0c3368e
danielicapui <daniel.icapui@hotmail.com>
2022-05-10 19:04:16 -0300

    Reescrevendo generics views

--- escola/cursos/urls.py
+++ escola/cursos/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     #path('<int:question_id>/vote/',views.cadastro,name='cadastro'),
     #path('cadastro/',cadastro,name='cadastro'),
     path('cursos/',CursosAPIView.as_view(),name='cursos'),
+    path('cursos/<int:pk>',CursoAPIView.as_view(),name='curso'),
+    path('cursos/<int:curso_pk>/avaliacoes/',AvaliacoesAPIView.as_view(),name='curso_avaliacoes'),
+    path('cursos/<int:curso_pk>/avaliacoes/<int:avaliacao_pk>/',AvaliacaoAPIView.as_view(),name='curso_avaliacao'),
     path('avaliacoes/',AvaliacoesAPIView.as_view(),name='avaliacoes'),
-     path('cursos/<int:pk>',CursoAPIView.as_view(),name='curso'),
-    path('avaliacoes/<int:pk>',AvaliacaoAPIView.as_view(),name='avaliacao'),
+    path('avaliacoes/<int:avaliacao_pk>',AvaliacaoAPIView.as_view(),name='avaliacao'),
 ]
--- escola/cursos/views.py
+++ escola/cursos/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
@@ -11,6 +12,13 @@ class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset=Avaliacao.objects.all()
     serializer_class=AvaliacaoSerializer
 
+    def get_queryset(self):
+        #kwargs pega o int:curso_pk da uri
+        id=self.kwargs.get('curso_pk')
+        if id:
+            return self.queryset.filter(curso_id=id)
+        return self.queryset.all()
+
 class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset=Curso.objects.all()
     serializer_class=CursoSerializer
@@ -18,3 +26,10 @@ class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset=Avaliacao.objects.all()
     serializer_class=AvaliacaoSerializer
+    
+    def get_object(self):
+        curso_pk=self.kwargs.get('curso_pk')
+        avaliacao_pk=self.kwargs.get('avaliacao_pk')
+        if curso_pk:
+            return get_object_or_404(self.get_queryset(),curso_id=curso_pk,pk=avaliacao_pk)
+        return get_object_or_404(self.get_queryset(),pk=avaliacao_pk)
