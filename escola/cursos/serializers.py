2d6fea95707627d452f45a4443d547a13bc995f3
danielicapui <daniel.icapui@hotmail.com>
2022-05-17 09:46:03 -0300

    Customizando a serialização dos dados

--- escola/cursos/serializers.py
+++ escola/cursos/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+from django.db.models import Avg
 from  .models import Curso, Avaliacao
+
 
 class AvaliacaoSerializer(serializers.ModelSerializer):
 
@@ -19,6 +21,12 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
             'criacao',
             'ativo'
         )
+
+    def validate_avaliacao(self,valor):
+        if valor in range(1,6):
+            return valor
+        raise serializers.ValidationError('A avaliação está fora do range de 1 a 5')
+
 class CursoSerializer(serializers.ModelSerializer):
     #Nested Relationship abordagem que mostra os dados e relações
     #avaliacoes=AvaliacaoSerializer(many=True,read_only=True)
@@ -28,6 +36,9 @@ class CursoSerializer(serializers.ModelSerializer):
     
     #Primary Key related Field abordagem 
     avaliacoes=serializers.PrimaryKeyRelatedField(many=True,read_only=True)
+
+    media_avaliacoes=serializers.SerializerMethodField()
+
     class Meta:
         model=Curso
         fields=(
@@ -36,5 +47,13 @@ class CursoSerializer(serializers.ModelSerializer):
             'url',
             'criacao',
             'ativo',
-            'avaliacoes'
+            'avaliacoes',
+            'media_avaliacoes'
         )
+        
+    def get_media_avaliacoes(self,obj):
+        media=obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+
+        if media is None:
+            return 0
+        return round(media*2)/2
